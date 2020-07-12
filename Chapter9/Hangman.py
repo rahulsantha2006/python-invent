5d28f669f58d869ad16804206c7d5af8ea656a71
rahulsantha2006 <rahul.santha.2006@gmail.com>
2020-07-12 14:24:47 -0500

    Working on hangman challenge

---
+++ Chapter9/Hangman.py
@@ -0,0 +1,72 @@
+import random
+hangmanpics = ['''
+
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+  =======''', '''
+  
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+  =======''','''
+  
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+  =======''','''
+  
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+ ========''', '''
+ 
+ +---+
+ |   |
+ O   |
+/|\  |
+     |
+     |
+ ========''','''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/    |
+     |
+ =======''', '''
+ 
+ +---+
+ |   |
+ O   |
+/|\  |
+/ \  |
+     |
+ =======''']
+
+words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar
+coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion
+lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon
+python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake
+spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat
+zebra'.split()
+
+def getRandomWord(wordlist):
+
+    wordIndex = random.randint(0, len(wordlist)- 1)
+    return wordlist[wordIndex]
+def displayBoard(hangmanpics, missedLetters, correctLetters, secretWord):
+    print()
