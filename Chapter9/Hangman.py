c684aa4f2a6c6f5f14a4850148e912a038c7d7e2
rahulsantha2006 <rahul.santha.2006@gmail.com>
2020-07-13 13:29:21 -0500

    Finished coding challenge yet to debug

--- Chapter9/Hangman.py
+++ Chapter9/Hangman.py
@@ -1,5 +1,5 @@
 import random
-hangmanpics = ['''
+HANGMANPICS = ['''
 
   +---+
   |   |
@@ -57,16 +57,105 @@ hangmanpics = ['''
      |
  =======''']
 
-words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar
+words = '''ant baboon badger bat bear beaver camel cat clam cobra cougar
 coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion
 lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon
 python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake
 spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat
-zebra'.split()
+zebra'''
+words.split()
 
 def getRandomWord(wordlist):
 
     wordIndex = random.randint(0, len(wordlist)- 1)
     return wordlist[wordIndex]
-def displayBoard(hangmanpics, missedLetters, correctLetters, secretWord):
+def displayBoard(HANGMANPICS, missedLetters, correctLetters, secretWord):
+    print(HANGMANPICS[len(missedLetters)])
     print()
+#this for loop will print the letters you have missed
+    print('Missed Letters:', end='')
+    for letter in missedLetters:
+        print(letter, end='')
+    print()
+# this for loop identifies the characters in the secret word and makes blanks accordingly
+# it will also replace any of the correctly guessed letters to the relating space
+    blanks = '_'*len(secretWord)
+
+    for i in range(len(secretWord)):
+
+        if secretWord[i] in correctLetters:
+            blanks = blanks[:i]+secretWord[i]+blanks[i+1:]
+# prints the secret word with spaces between each letter letter
+    for letter in blanks:
+
+        print(letter, end='')
+    print()
+
+#Makes sure the player enters a valid character as a input
+def getGuess(alreadyGuessed):
+
+    while True:
+        print('Guess a letter.')
+        guess = input()
+        guess = guess.lower()
+        if len(guess) !=1:
+            print('please enter a single letter.')
+        elif guess in alreadyGuessed:
+            print('You have already guessed that letter. Choose again.')
+        elif guess not in 'abcdefghijklmnopqrstuvxyz':
+            print('Please enter a LETTER.')
+        else:
+            return guess
+
+def playAgain():
+
+
+    print('Do you want to play again? (yes or no)')
+    return input().lower().startswith('y')
+
+
+print('H A N G M A N')
+missedLetters = ''
+correctLetters = ''
+secretWord = getRandomWord(words)
+gameIsDone = False
+
+while True:
+    displayBoard(HANGMANPICS,missedLetters,correctLetters,secretWord)
+#   lets the player type a letter
+    guess = getGuess(missedLetters+correctLetters)
+
+    if guess in secretWord:
+        correctLetters = correctLetters+guess
+
+
+        foundAllLetters = True
+        for i in range(len(secretWord)):
+            if secretWord[i] not in correctLetters:
+                foundAllLetters = False
+                break
+        if foundAllLetters:
+            print('Yes! The secret word is '+ secretWord+'! You have won')
+
+            gameIsDone = True
+    else:
+        missedLetters = missedLetters+guess
+
+
+        if len(missedLetters) == len(HANGMANPICS)-1:
+            displayBoard(HANGMANPICS,missedLetters,correctLetters,secretWord)
+
+            print('You have run out of guesses!\nAfter'+str(len(missedLetters))+
+                  'missed guesses and'+ str(len(correctLetters))+ ' correct guesses, the word was'
+                  + secretWord+'')
+            gameIsDone=True
+
+        if gameIsDone:
+            if playAgain():
+                missedLetters=''
+                correctLetters=''
+                gameIsDone=False
+                secretWord = getRandomWord(words)
+            else:
+                break
+
