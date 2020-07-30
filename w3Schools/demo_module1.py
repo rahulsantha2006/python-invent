0bd1270356e222d4af13fbd3a77b8f03a9ca1942
rahulsantha2006 <rahul.santha.2006@gmail.com>
2020-07-30 12:57:23 -0500

    learned about modules in python

---
+++ w3Schools/demo_module1.py
@@ -0,0 +1,6 @@
+import mymodule as blati
+
+blati.greeting("Jonathan")
+
+a =blati.person1["age"]
+print(a)
---
+++ w3Schools/mymodule.py
@@ -0,0 +1,8 @@
+def greeting(name):
+    print("Module captain is ", name)
+
+person1 = {
+    "name": "Rahul",
+     "age": 14,
+    "country": "U.S."
+}
